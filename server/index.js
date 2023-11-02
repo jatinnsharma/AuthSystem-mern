23d1a6b9c89f4b14035fd58fb5abf12a158420de
jatin <jatinsharma2231@gmail.com>
2023-11-03 00:41:18 +0530

    getting user data from frontend and sending to backend

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ app.use(express.urlencoded())
 
 const PORT = process.env.PORT 
 
+const data = []
+const userData = []
+
 app.get('/',(req,res)=>{
     res.send('hello this is home page')
 })
@@ -16,6 +19,33 @@ app.get('/about',(req,res)=>{
     res.send('Hello world !! this is about page. ')
 })
 
+app.post('/data',(req,res)=>{
+    const bookdata ={ 
+        title:req.body.title,
+        author:req.body.author
+    }
+    data.push(bookdata)
+    res.redirect('/getData')
+
+})
+
+app.post('/register',(req,res)=>{
+    const userInfo = {
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        phoneNumber:req.body.phoneNumber
+    }
+
+    userData.push(userInfo)
+    console.log(userData)
+})
+
+app.get('/getData',(req,res)=>{
+    res.json(data)
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
